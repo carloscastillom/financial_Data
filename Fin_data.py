cac00066b4af6c61db1b58625bacbb35f1b790b7
Carlos Castillo Meneses <65776444+carloscastillom@users.noreply.github.com>
2023-04-01 20:58:38 +0200

    Add files via upload

--- Fin_data.py
+++ Fin_data.py
@@ -2,11 +2,20 @@ import os
 import pandas as pd
 from yahooquery import Ticker
 
-
-symbols = ['fb', 'aapl', 'amzn', 'nflx', 'goog']
+symbols = ['aapl', 'amzn', 'nflx', 'goog', 'fb']
 
 for i in symbols:
+    directory = f'directory_{i}'
+    if not os.path.exists(directory):
+        os.makedirs(directory)
     tk = Ticker(i)
-    df = tk.income_statement()
-    print(df)
-    
+    try:
+        income_statement = tk.income_statement()
+        print(type(income_statement))  # print the data type of income_statement
+        print(income_statement)  # print the content of income_statement
+
+        file_path = os.path.join(directory, 'data.csv')
+        income_statement.to_csv(file_path)
+    except AttributeError:
+        print(f"Income statement data unavailable for {i}")
+    #print(df)
