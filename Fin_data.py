1101232a14481bee8f303d617878f7d65a163f9e
Carlos Castillo Meneses <65776444+carloscastillom@users.noreply.github.com>
2023-04-02 20:46:24 +0200

    Add files via upload

--- Fin_data.py
+++ Fin_data.py
@@ -1,11 +1,12 @@
 import os
 import pandas as pd
 from yahooquery import Ticker
+import time
 
-symbols = ['aapl', 'amzn', 'nflx', 'goog', 'fb']
-
+symbols = ['aapl', 'amzn', 'nflx', 'goog', 'fb', 'BYND']
+directory ='directory'
+start_time = time.time()  # record the start time
 for i in symbols:
-    directory = f'directory_{i}'
     if not os.path.exists(directory):
         os.makedirs(directory)
     tk = Ticker(i)
@@ -13,9 +14,12 @@ for i in symbols:
         income_statement = tk.income_statement()
         print(type(income_statement))  # print the data type of income_statement
         print(income_statement)  # print the content of income_statement
+        print(time.time())  # print the content of income_statement
 
-        file_path = os.path.join(directory, 'data.csv')
+        file_path = os.path.join(directory, f'{i}_.csv')
         income_statement.to_csv(file_path)
     except AttributeError:
         print(f"Income statement data unavailable for {i}")
-    #print(df)
+
+end_time = time.time()  # record the end time
+print(f"Total execution time: {end_time - start_time} seconds")  # print the total execution time
